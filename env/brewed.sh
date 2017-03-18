698b11e8ede63de88bbb787e3b643d4182b49bdd
Frédéric Le Mouël <frederic.le-mouel@insa-lyon.fr>
2017-03-18 19:46:04 +0100

    No need for dotfiles in my startup

--- env/brewed.sh
+++
@@ -1,9 +0,0 @@
-# Stuff from Homebrew-installed apps
-
-if [ -f `brew --prefix`/etc/bash_completion ]; then
-  . `brew --prefix`/etc/bash_completion
-fi
-
-if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
-
-complete -C aws_completer aws
--- env/main.sh
+++
@@ -1,8 +0,0 @@
-# Main environment variables and aliases
-
-export LC_ALL=en_US.UTF-8
-export LANG=en_US.UTF-8
-
-alias ls='ls -G'
-
-export EDITOR='nano -w'
